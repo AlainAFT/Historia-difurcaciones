706f2c2a42adde78bb4ee080195fe8bce24e71c7
Alain Anthony Flores Tapia <alainloco9@gmail.com>
2025-03-26 22:57:12 -0400

    Update Alain-Flores-practice.cpp

--- Alain-Flores-practice.cpp
+++ Alain-Flores-practice.cpp
@@ -14,8 +14,8 @@ int leerEleccion(string mensaje) {
     cout << mensaje;
     cin >> opcion;
     // Validar entrada básica
-    while (opcion != 1 && opcion != 2) {
-        cout << "Por favor, elige 1 o 2: ";
+    while (opcion != 1 && opcion != 2 && opcion!=3) {
+        cout << "Por favor, respeta y elige los numeros que te dan : ";
         cin >> opcion;
     }
     return opcion;
@@ -25,6 +25,7 @@ int main() {
     // Variables principales
     bool investigoSotano = false;
     bool cerroPuerta = false;
+    bool hechizos = false;
 
     // Introducción
     cout << "============================\n";
@@ -53,13 +54,18 @@ int main() {
     cout << "Escuchas ruidos extraños en el sótano.Parece venir del sotano.\n";
     cout << "1. Bajar a investigar.\n";
     cout << "2. Ignorar los ruidos.\n";
+    cout<<"3. seguir adelante y ver la sala principal."<<endl;
      int opcion = leerEleccion("Tu elección: ");
     if (opcion == 1) {
         investigoSotano = true;
         cout << "Encuentras un diario extraño con notas aterradoras.\n";
-        cout<<"Donde habla sobre sus debilidades de las criaturas"<<endl;
-    } else {
-        cout << "Deciden ignorar los ruidos y quedarse en la sala principal.\n";
+        cout<<"Donde habla sobre sus debilidades de las criaturas y sabes donde se encuentra la escopeta"<<endl;
+    } else if(opcion==2) {
+        cout << "Decides ignorar los ruidos y pasear por la cabaña.\n";
+    } else if(opcion==3){
+        hechizos=true;
+        cout<<"pillaste un libro de hechizos parece tener informacion valiosa para los mounstros"<<endl;
+    
     }
     pausar();
 
@@ -68,24 +74,73 @@ int main() {
     cout<<"¿Parecen ser ellos o son personas?"<<endl;
     cout << "1. Cerrar la puerta con llave.\n";
     cout << "2. Dejarla sin asegurar para no hacer ruido.\n";
+    cout<<"3. Dejarla abierta porque si "<<endl;
     opcion = leerEleccion("Tu elección: ");
-    cerroPuerta = (opcion == 1);
+    if(opcion==2 or opcion==3){
+        cerroPuerta=false;
+    }
 
     pausar();
+    
+   // enfrentamiento 
+    if(hechizos==1){
+        cout<<"Gracias al libro que encontraste pudiste dejar una restriccion en la cabaña del tal forma que no pudieron entrar los mounstros"<<endl;
+        cout<<"puedes vivir tranquilo, sobreviviste"<<endl;
+        return 0;
+    }
+    if ( cerroPuerta!=1 and investigoSotano==1){
+        int vidadelanimales=4;
+        int cartuchos=4;
+        cout<<"entraron las bestias apura y encuentra algo con que defenderte"<<endl;
+        cout<<"esta enfrente tuyo las criaturas necesitas defenderte, toma la  escopeta de al lado y dales varios headshots"<<endl;
+        cout<<"tienes que disparar para librarte de estas bestias"<<endl;
+        cout<<"solo te queda disparar presiona el numero uno para hacerlo y solo tienes 4 cartuchos"<<endl;
+        int opcion;
+        cin>>opcion;
+        if(opcion==1){
+        for(cartuchos;cartuchos>0;--vidadelanimales)
+         cartuchos= cartuchos-1;
+         cout<<"te quedan "<<vidadelanimales<<" y "<<cartuchos<<" cartuchos"<<endl;
+         if(vidadelanimales==0 ){
+             cout<<"mataste a todos los mounstros gracias a que sabias su lugares debiles"<<endl;
+         }
+    }
+    }
+    if(cerroPuerta!=1 and investigoSotano!=1){
+        int vidadelanimales=5;
+        int cartuchos=4;
+        cout<<"entraron las bestias apura y encuentra algo con que defenderte"<<endl;
+        cout<<"esta enfrente tuyo las criaturas necesitas defenderte, toma la  escopeta de al lado y dales varios headshots"<<endl;
+        cout<<"tienes que disparar para librarte de estas bestias"<<endl;
+        cout<<"solo te queda disparar presiona el numero uno para hacerlo y solo tienes 4 cartuchos"<<endl;
+        int opcion;
+        cin>>opcion;
+        if(opcion==1){
+        for(cartuchos;cartuchos>0;--vidadelanimales)
+         cartuchos= cartuchos-1;
+         cout<<"te quedanron "<<cartuchos<<" cartuchos y "<<vidadelanimales<<" mounstro"<<endl;
+         if(cartuchos==0){
+             cout<<"Fallaste el disparo, te quedo uno....."<<endl;
+             
+             cout<<"Aunque intentaste sobrevivir no fue lo suficiente para salir vivo"<<endl;
+         }
+        }
+         
+    }
 
     // Final básico basado en decisiones
     cout << "\n=== Resultado ===\n";
     if (investigoSotano && cerroPuerta) {
-        cout << "Lograste cerrar la puerta y aunque entraron , usaste la información del diario para sobrevivir.\n";
+        cout << "Lograste cerrar la puerta y aunque entraron , usaste la información del diario y la escopeta   para sobrevivir.\n";
         cout << "¡Sobreviviste!\n";
-    } else if (investigoSotano) {
-        cout << "Aunque sabías lo que enfrentabas, no cerraste la puerta no tuviste tiempo para defenderte y las criaturas te atraparon.\n";
+    } else if (investigoSotano && investigoSotano!=1) {
+        cout << "Aunque sabías lo que enfrentabas, fallaste el ultimo cartucho y las criaturas te atraparon.\n";
         cout << "Fin del juego.\n";
     } else if (cerroPuerta) {
         cout << "Cerraste la puerta, ganaste tiempo pero no sabias como enfrentarlos. No sobreviviste.\n";
         cout << "Fin del juego.\n";
-    } else {
-        cout << "No cerraste la puerta ni investigaste. Las criaturas te encontraron como una presa fácil.\n";
+    } else if(hechizos==false and investigoSotano==false and cerroPuerta==false) { 
+        cout << "No cerraste la puerta ni investigaste ni tenias algun hechizo. Las criaturas te encontraron como una presa fácil.\n";
         cout << "Fin del juego.\n";
     }
 
